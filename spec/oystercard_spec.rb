0815de4f7e7d169adc08f755442f9ef658f3d629
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-10-03 16:43:01 +0100

    Add balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,9 @@
-describe Oystercard
+require 'oystercard'
+
+describe Oystercard do
+
+  it 'has a starting balance of 0' do
+    oystercard = Oystercard.new
+    expect(oystercard.balance).to eq(0)
+  end
 end
